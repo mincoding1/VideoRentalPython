db917bee8c0d9a86533436eda9a15328e33e17eb
inho.choi <inho.choi@mincoding.co.kr>
2023-05-09 01:25:31 +0900

    3-7

--- customer.py
+++ customer.py
@@ -10,16 +10,25 @@ class Customer:
         return self.__name
 
     def statement(self):
-        result = "Rental Record for " + self.get_name() + "\n"
+        result = self.get_statement_header()
+        result += self.get_rental_line_report()
+        result += self.get_statement_footer()
+        return result
 
+    def get_statement_header(self):
+        result = "Rental Record for " + self.get_name() + "\n"
+        return result
+
+    def get_statement_footer(self):
+        result = "Amount owed is " + str(self.get_total_amount()) + "\n"
+        result += "You earned " + str(self.get_frequent_renter_points()) + " frequent renter points"
+        return result
+
+    def get_rental_line_report(self):
+        result = ""
         for each in self.__rentals:
             # show figures for this rental
             result += "\t" + each.get_movie().get_title() + "\t" + str(each.get_charge()) + "\n"
-
-        # add footer lines
-        result += "Amount owed is " + str(self.get_total_amount()) + "\n"
-        result += "You earned " + str(self.get_frequent_renter_points()) + " frequent renter points"
-
         return result
 
     def get_frequent_renter_points(self):
