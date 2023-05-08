a57dfba6bd3d474b503594dc70309cb1f991ad1f
JonJaryongLee <director9303@gmail.com>
2023-05-08 17:31:33 +0900

    1-3

--- test_movie_rental.py
+++ test_movie_rental.py
@@ -18,3 +18,22 @@ class Tests(TestCase):
                 'Rental Record for NAME_NOT_IMPORTANT\n'
                 + 'Amount owed is 0\n'
                 + 'You earned 0 frequent renter points')
+
+    def test_statement_for_regular_movie_rental_for_less_than_3_days(self):
+        #arrange
+        customer = Customer('NAME_NOT_IMPORTANT')
+        movie = Movie('TITLE_NOT_IMPORTANT', Movie.REGULAR)
+        days_rented = 2
+        rental = Rental(movie, days_rented)
+        customer.add_rental(rental)
+
+        #act
+        statement = customer.statement()
+
+        #assert
+        self.assertEqual(statement,
+                'Rental Record for NAME_NOT_IMPORTANT\n'
+                + '\tTITLE_NOT_IMPORTANT\t2.0\n'
+                + 'Amount owed is 2.0\n'
+                + 'You earned 1 frequent renter points')
+
