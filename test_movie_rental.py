84d49c282f0ec2b4e3cfb5bb499529cb08ca54e9
JonJaryongLee <director9303@gmail.com>
2023-05-08 17:31:20 +0900

    1-2

--- test_movie_rental.py
+++ test_movie_rental.py
@@ -5,3 +5,16 @@ class Tests(TestCase):
     def test_return_new_customer(self):
         customer = Customer('NAME_NOT_IMPORTANT')
         self.assertIsNotNone(customer)
+
+    def test_statement_for_no_rental(self):
+        #arrange
+        customer = Customer('NAME_NOT_IMPORTANT')
+
+        #act
+        statement = customer.statement()
+
+        #assert
+        self.assertEqual(statement,
+                'Rental Record for NAME_NOT_IMPORTANT\n'
+                + 'Amount owed is 0\n'
+                + 'You earned 0 frequent renter points')
